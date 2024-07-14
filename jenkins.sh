ecd3916021a216e1898c54ab366195b368864fe5
AKSHAY VINODRAO TIWASKAR <159642534+atiwaskar@users.noreply.github.com>
2024-07-14 14:15:16 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+chmod +x jenkins.sh
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre -y
+sudo systemctl start jenkins
